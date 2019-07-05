a32ac817b194f25d6ac54bd13317daa1eadb1b42
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-07-05 20:56:23 +0700

    Update purge.js

--- commands/server/purge.js
+++ commands/server/purge.js
@@ -35,7 +35,7 @@ class purgeCommand extends commando.Command {
         .addField("⏰ | Time", "-> " + message.createdAt)
         .setTimestamp()
         .setFooter("FrostedHost Bot | created by hieu#0843")}));
-    channel.bulkDelete(10).then(messages => moderationlogs.send({embed: new Discord.RichEmbed()
+    channel.bulkDelete(numberofmessagesdeleted).then(messages => moderationlogs.send({embed: new Discord.RichEmbed()
         .setTitle("**FrostedHost | Purge**")
         .setColor("#4286f4")
         .addField("📌 | Executor", message.author.tag)
